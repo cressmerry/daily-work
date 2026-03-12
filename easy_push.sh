eed5c5108979975246d82bfbd1cb0319aa0cd40a
cressmerry <232644700+cressmerry@users.noreply.github.com>
2026-03-12 10:49:20 +0530

    test2

--- easy_push.sh
+++ easy_push.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-echo "==================================================================="
-echo ""
-echo ""
-git add .
-git commit -m "$1"
-git push -u origin main
-echo ""
-echo ""
-echo "==================================================================="
+
+if [ $# -eq 0 ]; then
+    echo "No commit mssage provided. Please provide one."
+else
+	echo "===================================================================\n\n"
+	git add .
+	git commit -m "$1"
+	git push -u origin main
+	echo "\n==================================================================="
+fi
 
 echo "Exiting Script...."
