b734871db919bed082f2ece17e85c177bfe983f2
cressmerry <232644700+cressmerry@users.noreply.github.com>
2026-04-17 12:14:29 +0530

    Implemented User and  Role entities

---
+++ order-api/src/main/java/com/tek/order/entity/ERole.java
@@ -0,0 +1,7 @@
+package com.tek.order.entity;
+
+public enum ERole {
+	ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
--- order-api/src/main/java/com/tek/order/service/UserDetailsServiceImplementation.java
+++ order-api/src/main/java/com/tek/order/service/UserDetailsServiceImplementation.java
@@ -1,9 +1,26 @@
 package com.tek.order.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class UserDetailsServiceImplementation implements UserDetailsService{
+	@Autowired
+	UserRespository userRepository;
 	
+	@Override
+	@Transactional
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+		User user = userRepository.findByUsername(username)
+				.orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+		return UserDetailsImplementation.build(user);
+	}
 }
