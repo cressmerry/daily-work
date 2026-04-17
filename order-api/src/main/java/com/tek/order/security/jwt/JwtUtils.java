745f72c7055b3a70416127243bba83dafe81c674
cressmerry <232644700+cressmerry@users.noreply.github.com>
2026-04-17 14:47:26 +0530

    final fix

--- order-api/src/main/java/com/tek/order/security/jwt/JwtUtils.java
+++ order-api/src/main/java/com/tek/order/security/jwt/JwtUtils.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+
 import com.tek.order.service.UserDetailsImplementation;
 
 import io.jsonwebtoken.*;
@@ -15,50 +16,55 @@ import java.nio.charset.StandardCharsets;
 
 @Component
 public class JwtUtils {
-	private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
-	
-	@Value("${com.tek.security.jwt.jwtSecret}")
-	private String jwtSecret;
-	
-	@Value("${com.tek.security.jwt.jwtExpirationMs}")
-	private int jwtExpirationMs;
-	
-	private Key getSigningKey() {
-		byte[] keyBytes = this.jwtSecret.getBytes(StandardCharsets.UTF_8);
-		return Keys.hmacShaKeyFor(keyBytes);
-	}	
-	
-	public String generateJwtToken(Authentication authentication) {
-		UserDetailsImplementation userPrincipal = (UserDetailsImplementation) authentication.getPrincipal();
+    private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
-		return Jwts.builder()
-				.setSubject((userPrincipal.getUsername()))
-				.setIssuedAt(new Date())
-				.setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
-				.signWith(getSigningKey(), SignatureAlgorithm.HS512) 
-				.compact();
-	}
-	
-	public boolean validateJwtToken(String authToken) {
-		try {
-			// Universal 0.10.x / 0.11.x parser logic
-			Jwts.parserBuilder()
-				.setSigningKey(getSigningKey())
-				.build()
-				.parseClaimsJws(authToken);
-			return true;
-		} catch (Exception e) {
-			logger.error("JWT Error: {}", e.getMessage());
-		}
-		return false;
-	}
-	
-	public String getUsernameFromJwtToken(String authToken) {
-		return Jwts.parserBuilder()
-				.setSigningKey(getSigningKey())
-				.build()
-				.parseClaimsJws(authToken)
-				.getBody()
-				.getSubject();
-	}
+    @Value("${com.tek.security.jwt.jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${com.tek.security.jwt.jwtExpirationMs}")
+    private int jwtExpirationMs;
+
+    private Key getSigningKey() {
+        byte[] keyBytes = this.jwtSecret.getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateJwtToken(Authentication authentication) {
+        UserDetailsImplementation userPrincipal = (UserDetailsImplementation) authentication.getPrincipal();
+
+        return Jwts.builder()
+                .setSubject((userPrincipal.getUsername()))
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    public boolean validateJwtToken(String authToken) {
+        try {
+            Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(authToken);
+            return true;
+        } catch (SecurityException | MalformedJwtException e) {
+            logger.error("Invalid JWT token: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            logger.error("JWT token is expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            logger.error("JWT token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            logger.error("JWT claims string is empty: {}", e.getMessage());
+        }
+        return false;
+    }
+
+    public String getUsernameFromJwtToken(String authToken) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(authToken)
+                .getBody()
+                .getSubject();
+    }
 }
