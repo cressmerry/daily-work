73e467e9424070194d3db2c5872e0781583bae21
cressmerry <232644700+cressmerry@users.noreply.github.com>
2026-04-20 14:20:44 +0530

    Unified CORS in WebSecurityConfig.java

--- order-api/src/main/java/com/tek/order/controller/AuthController.java
+++ order-api/src/main/java/com/tek/order/controller/AuthController.java
@@ -14,7 +14,6 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -85,8 +84,6 @@ public class AuthController {
 					.badRequest()
 					.body(new MessageResponse("Error: Email is already in use!"));
 		}
-
-		// Create new user's account
 		User user = new User(signUpRequest.getUsername(), 
 							 signUpRequest.getEmail(),
 							 encoder.encode(signUpRequest.getPassword()));
